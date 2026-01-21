19ef34ac8209bffa27f07fb75d7d75779f393cf6
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-01-20 17:57:47 -0800

    Add conversion helper methods

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,21 @@ pub mod moves;
 pub mod rende;
 pub mod util;
 
+use crate::bitboards::bitboard_constants::bitboard_indices::*;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Color {
     Black,
     White,
+}
+
+impl Color {
+    pub fn to_index(&self) -> usize {
+        match self {
+            Color::White => WHITE,
+            Color::Black => BLACK,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -18,4 +29,17 @@ pub enum Piece {
     Bishop,
     Queen,
     King,
+}
+
+impl Piece {
+    pub fn to_index(&self) -> usize {
+        match self {
+            Piece::Pawn => PAWN,
+            Piece::Rook => ROOK,
+            Piece::Knight => KNIGHT,
+            Piece::Bishop => BISHOP,
+            Piece::Queen => QUEEN,
+            Piece::King => KING,
+        }
+    }
 }
