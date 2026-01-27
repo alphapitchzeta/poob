cedbca53a538bc7775f979bb8497826d71682ae5
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-01-27 12:19:46 -0800

    Add types for move ordering
    Also added documentation

--- src/moves.rs
+++ src/moves.rs
@@ -1,5 +1,5 @@
-// Constants representing the different arrangements of bit flags
-// for the four leftmost bits of a u16 representing a move.
+/// Constants representing the different arrangements of bitflags
+/// for the four leftmost bits of a u16 representing a move.
 pub mod move_constants {
     pub const QUIET_MOVE: u16 = 0b0000 << 12;
     pub const DOUBLE_PAWN_PUSH: u16 = 0b0001 << 12;
@@ -18,9 +18,14 @@ pub mod move_constants {
     pub const QUEEN_PROMOTION_CAPTURE: u16 = 0b1111 << 12;
 }
 
+/// The maximum possible moves from any given chess position.
+const MAX_POSSIBLE_MOVES: usize = 218;
+
 use crate::util::*;
 use move_constants::*;
 
+/// Struct encapsulating the logic for encoding and decoding moves.
+/// All information is stored in a `u16` field.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Move(u16);
 
@@ -55,64 +60,208 @@ impl Move {
         (self.0 & 0b111111) as u8
     }
 
+    /// Returns `true` if the `QUIET_MOVE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_quiet(&self) -> bool {
         (self.0 >> 12) & QUIET_MOVE == QUIET_MOVE
     }
 
+    /// Returns `true` if the `DOUBLE_PAWN_PUSH` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_double_pawn_push(&self) -> bool {
         self.0 & DOUBLE_PAWN_PUSH == DOUBLE_PAWN_PUSH
     }
 
+    /// Returns `true` if the `KING_CASTLE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_kingside_castle(&self) -> bool {
         self.0 & KING_CASTLE == KING_CASTLE
     }
 
+    /// Returns `true` if the `QUEEN_CASTLE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_queenside_castle(&self) -> bool {
         self.0 & QUEEN_CASTLE == QUEEN_CASTLE
     }
 
+    /// Returns `true` if the `CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_capture(&self) -> bool {
         self.0 & CAPTURE == CAPTURE
     }
 
+    /// Returns `true` if the `EN_PASSANT_CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_en_passant_capture(&self) -> bool {
         self.0 & EN_PASSANT_CAPTURE == EN_PASSANT_CAPTURE
     }
 
+    /// Returns `true` if the `PROMOTION` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_promotion(&self) -> bool {
         self.0 & PROMOTION == PROMOTION
     }
 
+    /// Returns `true` if the `KNIGHT_PROMOTION` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_knight_promotion(&self) -> bool {
         self.0 & KNIGHT_PROMOTION == KNIGHT_PROMOTION
     }
 
+    /// Returns `true` if the `BISHOP_PROMOTION` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_bishop_promotion(&self) -> bool {
         self.0 & BISHOP_PROMOTION == BISHOP_PROMOTION
     }
 
+    /// Returns `true` if the `ROOK_PROMOTION` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_rook_promotion(&self) -> bool {
         self.0 & ROOK_PROMOTION == ROOK_PROMOTION
     }
 
+    /// Returns `true` if the `QUEEN_PROMOTION` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_queen_promotion(&self) -> bool {
         self.0 & QUEEN_PROMOTION == QUEEN_PROMOTION
     }
 
+    /// Returns `true` if the `KNIGHT_PROMOTION_CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_knight_promotion_capture(&self) -> bool {
         self.0 & KNIGHT_PROMOTION_CAPTURE == KNIGHT_PROMOTION_CAPTURE
     }
 
+    /// Returns `true` if the `BISHOP_PROMOTION_CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_bishop_promotion_capture(&self) -> bool {
         self.0 & BISHOP_PROMOTION_CAPTURE == BISHOP_PROMOTION_CAPTURE
     }
 
+    /// Returns `true` if the `ROOK_PROMOTION_CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_rook_promotion_capture(&self) -> bool {
         self.0 & ROOK_PROMOTION_CAPTURE == ROOK_PROMOTION_CAPTURE
     }
 
+    /// Returns `true` if the `QUEEN_PROMOTION_CAPTURE` bitflag is set, and `false`
+    /// otherwise.
     pub fn is_queen_promotion_capture(&self) -> bool {
         self.0 & QUEEN_PROMOTION_CAPTURE == QUEEN_PROMOTION_CAPTURE
+    }
+}
+
+/// Struct representing a `Move` and its corresponding score.
+#[derive(Debug, Clone, Copy)]
+pub struct MoveScore {
+    pub mv: Move,
+    pub score: i32,
+}
+
+impl MoveScore {
+    pub fn default() -> Self {
+        Self {
+            mv: Move(0),
+            score: 0,
+        }
+    }
+
+    pub fn new(mv: Move) -> Self {
+        Self {
+            mv,
+            score: 0,
+        }
+    }
+
+    pub fn with_score(mv: Move, score: i32) -> Self {
+        Self {
+            mv,
+            score,
+        }
+    }
+}
+
+/// A custom implementation of an `ArrayVec`, storing a collection
+/// of `MoveScore`.
+pub struct MoveList {
+    list: [MoveScore; MAX_POSSIBLE_MOVES],
+    len: usize,
+}
+
+impl MoveList {
+    pub fn new() -> Self {
+        Self {
+            list: [MoveScore::default(); MAX_POSSIBLE_MOVES],
+            len: 0,
+        }
+    }
+
+    /// Returns the current length of the `MoveList`.
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Returns the capacity of the `MoveList` minus its current length.
+    pub fn spare_capacity(&self) -> usize {
+        MAX_POSSIBLE_MOVES - self.len
+    }
+
+    /// Sorts the `MoveScore` elements in the `MoveList` by their scores. Currently,
+    /// this sort is unstable.
+    pub fn sort(&mut self) {
+        self.list[0..self.len].sort_unstable_by_key(|mv| mv.score);
+    }
+
+    pub fn push(&mut self, entry: MoveScore) {
+        self.list[self.len] = entry;
+        self.len += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<MoveScore> {
+        let popped = *self.list.get(self.len - 1)?;
+
+        self.len -= 1;
+
+        Some(popped)
+    }
+
+    /// Returns a `Some(MoveScore)` of the `MoveScore` entry at the
+    /// corresponding index, or `None` if the index is out of bounds.
+    pub fn get(&self, index: usize) -> Option<MoveScore> {
+        if index >= self.len {
+            return None;
+        }
+
+        Some(self.list[index])
+    }
+
+    /// Returns a `Some(Move)` of the `MoveScore` entry at the
+    /// corresponding index, or `None` if the index is out of bounds.
+    pub fn get_move(&self, index: usize) -> Option<Move> {
+        let move_score = self.get(index)?;
+
+        Some(move_score.mv)
+    }
+
+    /// Calls `sort()` on the `MoveList` and returns the `MoveScore`
+    /// entry with the highest score.
+    /// 
+    /// # Panics
+    /// Currently directly indexes the underlying array. The index will
+    /// be out of bounds if the current length is 0.
+    pub fn get_best(&mut self) -> MoveScore {
+        self.sort();
+
+        self.list[self.len - 1]
+    }
+
+    /// Calls `sort()` on the `MoveList` and returns the `Move` of the
+    /// `MoveScore` entry with the highest score.
+    /// 
+    /// # Panics
+    /// Calls `get_best()`, which panics.
+    pub fn get_best_move(&mut self) -> Move {
+        self.get_best().mv
     }
 }
 
