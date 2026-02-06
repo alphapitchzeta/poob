b5641fef0929b6be968d2db937edc8b8ff77b10c
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-02-05 19:48:12 -0800

    Alter board printing

--- src/rende.rs
+++ src/rende.rs
@@ -5,51 +5,51 @@ use crate::bitboards::BitBoards;
 pub fn print_bitboard(bitboard: &BitBoards, perspective: Color) {
     let mut board_arr = [[' '; 8]; 8];
 
-    set_pieces(&mut board_arr, bitboard.pawns_white(), '♟');
-    set_pieces(&mut board_arr, bitboard.pawns_black(), '♙');
-    set_pieces(&mut board_arr, bitboard.rooks_white(), '♜');
-    set_pieces(&mut board_arr, bitboard.rooks_black(), '♖');
-    set_pieces(&mut board_arr, bitboard.knights_white(), '♞');
-    set_pieces(&mut board_arr, bitboard.knights_black(), '♘');
-    set_pieces(&mut board_arr, bitboard.bishops_white(), '♝');
-    set_pieces(&mut board_arr, bitboard.bishops_black(), '♗');
-    set_pieces(&mut board_arr, bitboard.queens_white(), '♛');
-    set_pieces(&mut board_arr, bitboard.queens_black(), '♕');
-    set_pieces(&mut board_arr, bitboard.king_white(), '♚');
-    set_pieces(&mut board_arr, bitboard.king_black(), '♔');
-
-    print!("  ");
-
-    for c in 'a'..='h' {
-        print!(" {c} ");
-    }
-
-    print!("\n");
+    set_pieces(&mut board_arr, bitboard.pawns_white(), 'P');
+    set_pieces(&mut board_arr, bitboard.pawns_black(), 'p');
+    set_pieces(&mut board_arr, bitboard.rooks_white(), 'R');
+    set_pieces(&mut board_arr, bitboard.rooks_black(), 'r');
+    set_pieces(&mut board_arr, bitboard.knights_white(), 'N');
+    set_pieces(&mut board_arr, bitboard.knights_black(), 'n');
+    set_pieces(&mut board_arr, bitboard.bishops_white(), 'B');
+    set_pieces(&mut board_arr, bitboard.bishops_black(), 'b');
+    set_pieces(&mut board_arr, bitboard.queens_white(), 'Q');
+    set_pieces(&mut board_arr, bitboard.queens_black(), 'q');
+    set_pieces(&mut board_arr, bitboard.king_white(), 'K');
+    set_pieces(&mut board_arr, bitboard.king_black(), 'k');
 
     match perspective {
         Color::White => {
             for (rank, name) in board_arr.iter().rev().zip((1..=8).rev()) {
-                print!("{name} ");
-
                 for square in rank.iter() {
                     print!("[{}]", square);
                 }
+
+                print!(" {name}");
 
                 print!("\n");
             }
         }
         Color::Black => {
             for (rank, name) in board_arr.iter().zip(1..=8) {
-                print!("{name} ");
-
                 for square in rank.iter() {
                     print!("[{}]", square);
                 }
+
+                print!(" {name}");
 
                 print!("\n");
             }
         }
     };
+
+    //print!("  ");
+
+    for c in 'a'..='h' {
+        print!(" {c} ");
+    }
+
+    print!("\n");
 }
 
 fn set_pieces(board: &mut [[char; 8]; 8], bitboard: BitBoard, c: char) {
