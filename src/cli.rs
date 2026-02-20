d6861536fb690af1c3dbb485f6f75306a6e9e760
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-02-19 21:14:01 -0800

    Implement go command parsing

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,9 @@
 use std::io;
-use std::str::FromStr;
+use std::time::Instant;
+use std::str::{FromStr, SplitAsciiWhitespace};
 
-use crate::moves::*;
-use crate::{boardstate::*, game::*, perft::*};
+use crate::search::{MAX_PLY, MAX_TIME, time_ctrl::Timer};
+use crate::{Color, boardstate::*, game::*, moves::*, perft::*};
 
 pub const ID: &str = "Poob 0.1.0 by alphapitchzeta";
 
@@ -62,7 +63,7 @@ impl Session {
             let command = match buf.trim().parse::<Command>() {
                 Ok(c) => c,
                 Err(e) => {
-                    println!("Error: {e:?}");
+                    eprintln!("Error: {e:?}");
                     continue;
                 }
             };
@@ -70,7 +71,7 @@ impl Session {
             match self.execute(command) {
                 Ok(true) => (),
                 Ok(false) => break,
-                Err(e) => println!("Error: {e:?}"),
+                Err(e) => eprintln!("Error: {e:?}"),
             };
         }
     }
@@ -97,7 +98,7 @@ impl Session {
             }
             Command::IsReady => println!("readyok"),
             Command::UciNewGame => (),
-            Command::Go(_) => self.go(),
+            Command::Go(go_command) => self.go(go_command),
         };
 
         Ok(true)
@@ -124,8 +125,20 @@ impl Session {
         println!("Total nodes: {}", nodes);
     }
 
-    fn go(&self) {
-        let best_move = self.game.search_id();
+    fn go(&self, go_command: GoCommand) {
+        let (clock_set, increment_set) = match self.game.side_to_move() {
+            Color::White => (go_command.white_time, go_command.white_increment),
+            Color::Black => (go_command.black_time, go_command.black_increment),
+        };
+
+        let search_time = match clock_set {
+            Some(clock) => clock / 20 + increment_set.unwrap_or(0) / 2,
+            None => go_command.move_time,
+        }.clamp(0, go_command.move_time);
+
+        let search_timer = Timer::new(search_time, Instant::now());
+
+        let best_move = self.game.search(go_command.max_ply * 2, search_timer);
 
         println!("bestmove {}", best_move.to_string());
     }
@@ -148,7 +161,7 @@ impl Session {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub enum Command {
     SetPosition(Position),
     Perft(usize),
@@ -157,7 +170,7 @@ pub enum Command {
     Uci,
     UciNewGame,
     IsReady,
-    Go(Option<String>),
+    Go(GoCommand),
     Exit,
 }
 
@@ -170,6 +183,7 @@ pub enum ParseCommandError {
     NoMove,
     ParseMoveError(ParseMoveError),
     ParsePositionError(ParsePositionError),
+    ParseGoError(ParseGoError),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -217,7 +231,9 @@ impl FromStr for Command {
             "go" => {
                 let args = chunks.map(|s| s.to_string()).collect::<Vec<_>>().join(" ");
 
-                let real_args = if args.is_empty() { None } else { Some(args) };
+                let real_args = args
+                    .parse()
+                    .map_err(|e| ParseCommandError::ParseGoError(e))?;
 
                 Ok(Command::Go(real_args))
             }
@@ -269,5 +285,128 @@ impl FromStr for Position {
             ))),
             _ => Err(ParsePositionError::BadPosition),
         }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ParseGoError {
+    ParseIntError,
+    InvalidArgument,
+    MissingArgument,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct GoCommand {
+    pub move_time: i32,
+    pub max_ply: u16,
+    pub white_time: Option<i32>,
+    pub black_time: Option<i32>,
+    pub white_increment: Option<i32>,
+    pub black_increment: Option<i32>,
+}
+
+impl FromStr for GoCommand {
+    type Err = ParseGoError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chunks = s.split_ascii_whitespace();
+
+        let chunk_1 = chunks.next();
+
+        let mut go_command = GoCommand::default();
+
+        match chunk_1 {
+            None | Some("infinite") => return Ok(go_command),
+            Some("movetime") => go_command.move_time = parse_helper(&mut chunks)?,
+            Some("depth") => go_command.max_ply = parse_helper(&mut chunks)?,
+            Some("wtime") => go_command.white_time = Some(parse_helper(&mut chunks)?),
+            Some("btime") => go_command.black_time = Some(parse_helper(&mut chunks)?),
+            Some("winc") => go_command.white_increment = Some(parse_helper(&mut chunks)?),
+            Some("binc") => go_command.black_increment = Some(parse_helper(&mut chunks)?),
+            _ => return Err(ParseGoError::InvalidArgument),
+        };
+
+        while let Some(chunk) = chunks.next() {
+            match chunk {
+                "movetime" => go_command.move_time = parse_helper(&mut chunks)?,
+                "depth" => go_command.max_ply = parse_helper(&mut chunks)?,
+                "wtime" => go_command.white_time = Some(parse_helper(&mut chunks)?),
+                "btime" => go_command.black_time = Some(parse_helper(&mut chunks)?),
+                "winc" => go_command.white_increment = Some(parse_helper(&mut chunks)?),
+                "binc" => go_command.black_increment = Some(parse_helper(&mut chunks)?),
+                _ => return Err(ParseGoError::InvalidArgument),
+            };
+        }
+
+        Ok(go_command)
+    }
+}
+
+fn parse_helper<I: FromStr>(iter: &mut SplitAsciiWhitespace) -> Result<I, ParseGoError> {
+    Ok(iter
+        .next()
+        .ok_or(ParseGoError::MissingArgument)?
+        .parse()
+        .map_err(|_| ParseGoError::ParseIntError)?)
+}
+
+impl GoCommand {
+    pub fn default() -> Self {
+        Self {
+            move_time: MAX_TIME,
+            max_ply: MAX_PLY,
+            white_time: None,
+            black_time: None,
+            white_increment: None,
+            black_increment: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_go_parsing() {
+        let mut control_command = GoCommand::default();
+
+        let default_args = ["", "infinite"];
+
+        for arg in default_args {
+            let test_command = arg.parse().expect("malformed argument");
+            assert_eq!(control_command, test_command);
+        }
+
+        {
+            let bad_args = [
+                ("lol", Err(ParseGoError::InvalidArgument)),
+                ("wtime lmao", Err(ParseGoError::ParseIntError)),
+                ("binc", Err(ParseGoError::MissingArgument)),
+            ];
+
+            for bad_arg in bad_args {
+                let result = bad_arg.0.parse::<GoCommand>();
+                assert_eq!(result, bad_arg.1);
+            }
+        }
+
+        let test_args = (60000, 2, 6767, 2557, 2000, 2000);
+
+        control_command.move_time = test_args.0;
+        control_command.max_ply = test_args.1;
+        control_command.white_time = Some(test_args.2);
+        control_command.black_time = Some(test_args.3);
+        control_command.white_increment = Some(test_args.4);
+        control_command.black_increment = Some(test_args.5);
+
+        let test_arg = format!(
+            "movetime {} depth {} wtime {} btime {} winc {} binc {}",
+            test_args.0, test_args.1, test_args.2, test_args.3, test_args.4, test_args.5
+        );
+
+        let test_command = test_arg.parse().expect("malformed arguments");
+
+        assert_eq!(control_command, test_command);
     }
 }
